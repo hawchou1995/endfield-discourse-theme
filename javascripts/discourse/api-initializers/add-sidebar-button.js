bbce3d38a14d0c2042356e83133dc2fffeaacceb
hawchou1995 <45247266+hawchou1995@users.noreply.github.com>
2026-02-08 15:12:07 +0800

    Update add-sidebar-button.js

--- javascripts/discourse/api-initializers/add-sidebar-button.js
+++ javascripts/discourse/api-initializers/add-sidebar-button.js
@@ -2,9 +2,7 @@ import { apiInitializer } from "discourse/lib/api";
 import EndfieldNewTopicButton from "../components/endfield-new-topic-button";
 
 export default apiInitializer("1.0", (api) => {
-  // 确保用户登录
-  if (api.getCurrentUser()) {
-    // 渲染到侧边栏导航列表的正上方，这个位置在侧边栏内部
-    api.renderInOutlet("above-sidebar-sections", EndfieldNewTopicButton);
-  }
+  // 移除所有条件判断，强制渲染！
+  // 无论如何，先把占位符放上去，显示与否交给组件内部逻辑
+  api.renderInOutlet("above-sidebar-sections", EndfieldNewTopicButton);
 });
