987d79932f15a3e9a71c9b811964b9a46c5168d3
hawchou1995 <45247266+hawchou1995@users.noreply.github.com>
2026-02-08 14:54:39 +0800

    Update add-sidebar-button.js

--- javascripts/discourse/api-initializers/add-sidebar-button.js
+++ javascripts/discourse/api-initializers/add-sidebar-button.js
@@ -2,9 +2,8 @@ import { apiInitializer } from "discourse/lib/api";
 import EndfieldNewTopicButton from "../components/endfield-new-topic-button";
 
 export default apiInitializer("1.0", (api) => {
-  // 核心作业：使用 before-sidebar-columns
-  // 这个位置在 HTML 结构上是和侧边栏并列的，我们需要用 CSS 把它"塞"进侧边栏的格子里
   if (api.getCurrentUser()) {
-    api.renderInOutlet("before-sidebar-columns", EndfieldNewTopicButton);
+    // 回归最稳妥的插槽，它位于侧边栏导航列表的正上方
+    api.renderInOutlet("above-sidebar-sections", EndfieldNewTopicButton);
   }
 });
